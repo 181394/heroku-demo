28cb86b443a57312b6dc12efc01c0e4055b0b10f
Preben <181394@stud.hvl.no>
2018-11-07 17:16:26 +0100

    this is change

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-		    LOLOLLOLOLOLOLOLOLOLLO
+		    This is new smeashj!
           </p>
           <a
             className="App-link"
